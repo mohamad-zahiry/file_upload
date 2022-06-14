a5047b3b69ef38a1bee9cb3fdeb183cc0f292713
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-06-14 20:08:46 +0430

    Create get_file index view

--- get_file/views.py
+++ get_file/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from .forms import FileUploadForm
+from .utils import save_uploaded_file
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {}
+
+    if request.method == "POST":
+        form = FileUploadForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            file = request.FILES["file"]
+            save_uploaded_file(file, file.name)
+            context["success"] = True
+
+    else:
+        form = FileUploadForm()
+
+    context["form"] = form
+    return render(request, "index.html", context)
