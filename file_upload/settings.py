5329c362e8a72a601c5cca466569efe98f5945a3
Soheyl Zahiry <soheylzahiry@gmail.com>
2022-06-14 20:33:11 +0430

    Allow all hosts to ALLOWED_HOSTS

--- file_upload/settings.py
+++ file_upload/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-^923@&c@jw134x&@a8r-501$pz68oas9s7j-d7@5_3fg#d9&z+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+    ]
 
 
 # Application definition
